c0afa7de3af7f80e7b32920002d27613676482af
Thomas_Alan <nzampos@gmail.com>
2024-10-11 17:01:53 +0300

    swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -35,10 +40,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// routes
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs API</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
 });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
 app.use("/api/v1/auth", authRouter); //domain/api/v1/auth
 app.use("/api/v1/jobs", authenticateUser, jobsRouter); //domain/api/v1/jobs
 
